e7036c6547d1530766ee25c893f3a0c8d63bbcf3
Iffat <iamagduri@gmail.com>
2025-11-05 11:31:40 -0500

    update the patientListScreen

--- Screens/PatientListScreen.js
+++ Screens/PatientListScreen.js
@@ -1,20 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
+import { useState, useEffect } from 'react';
 
-export default function PatientListScreen() {
+
+import { BACKEND_URL } from '../config';  
+
+
+
+const PatientListScreen = ({ navigation }) => { 
+
+const [patients, setPatients] = useState([]);
+
+useEffect(() => {
+  const fetchPatients = async () => {
+    try {
+      const response = await fetch(`${BACKEND_URL}/patients`);
+      const data = await response.json();
+      setPatients(data);
+    } catch (error) {
+      console.error('Error fetching patients:', error);
+    }
+  };
+
+  fetchPatients();
+}, []);
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Patient List</Text>
+  
+      <FlatList //render patient list
+        data={patients}
+        keyExtractor={(item) => item._id.toString()}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+          style={styles.patientItem}
+          onPress={() => navigation.navigate('PatientInfo', { patient: item })}>
+      <Text>{item.firstName} {item.lastName}</Text>
+      <Text>{item.gender} | {item.phone}</Text>
+    </TouchableOpacity>
+        )}
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
  container: {
     flex: 1,
     backgroundColor: '#87d7f7ff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50, 
+    //alignItems: 'center',
+    //justifyContent: 'center',
+  },
+  patientItem: {
+    backgroundColor: 'white',
+    padding: 15,
+    borderRadius: 10,
+    marginVertical: 5,
+    marginHorizontal: 12,
+  },
+  patientName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  patientDetails: {
+    fontSize: 14,
+    color: '#555',
+    marginTop: 6,
   },
 });
+
+export default PatientListScreen;
