a34b301cb3370a02eb7ec43a7782064f7fcf4d7a
Iffat <iamagduri@gmail.com>
2025-12-06 18:51:02 -0500

    add patient search and critical dashboard in Home Screen

---
+++ Components/SearchContainer.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import Feather from '@react-native-vector-icons/feather';
+
+const SearchContainer = ({ value, onChangeText, onClear }) => {
+    //const [searchPatient, setSearchPatient] = React.useState('');
+
+    return (
+
+
+        <View style={styles.searchContainer}>
+            <Feather name="search" size={20} color="#666" style={styles.searchIcon} />
+            <TextInput
+                style={styles.searchBar}
+                placeholder="Search patient by name..."
+                value={value}
+                onChangeText={onChangeText}
+            />
+            {/* Clear button if text exists */}
+            {value.length > 0 && (
+                <TouchableOpacity onPress={() => onClear()}>
+                    <Feather name="x" size={20} color="#666" />
+                </TouchableOpacity>
+            )}
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+     searchContainer: {
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderRadius: 12,
+    padding: 12,
+    alignItems: 'center',
+    marginBottom: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  searchBar: {
+    flex: 1,
+    fontSize: 16,
+    color: '#333',
+    marginLeft: 10,
+  },
+});
+export default SearchContainer;
